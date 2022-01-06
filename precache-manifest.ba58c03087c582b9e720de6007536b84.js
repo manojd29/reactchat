f17fb7bfcaf1f95c721af454f02669c1e49e6d4f
Manoj <Manojd2905@gmail.com>
2022-01-06 20:06:00 +0530

    Updates

---
+++ precache-manifest.ba58c03087c582b9e720de6007536b84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5828c7cb14ebd97733465bad168e6e74",
+    "url": "/reactchat/index.html"
+  },
+  {
+    "revision": "eb4cd1e38f39bddbcb1e",
+    "url": "/reactchat/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "388c461f72afe3420074",
+    "url": "/reactchat/static/css/main.635784a2.chunk.css"
+  },
+  {
+    "revision": "eb4cd1e38f39bddbcb1e",
+    "url": "/reactchat/static/js/2.8b75aca5.chunk.js"
+  },
+  {
+    "revision": "f2f3c989bc1a24fc5b1bf09a05f1307d",
+    "url": "/reactchat/static/js/2.8b75aca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388c461f72afe3420074",
+    "url": "/reactchat/static/js/main.7c6b6426.chunk.js"
+  },
+  {
+    "revision": "39f5da35bcef15f5f0fe",
+    "url": "/reactchat/static/js/runtime-main.fb0bacbc.js"
+  },
+  {
+    "revision": "c93bb591e1a84d284158ff5727ea2e7d",
+    "url": "/reactchat/static/media/login.c93bb591.jpg"
+  }
+]);
